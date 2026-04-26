fe1534eaef393039a122ee33a8306a47098a3356
Chandan Meher <mchandan1204@gmail.com>
2026-04-26 16:00:47 +0530

    feat: add @vercel/analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,14 +28,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en">
-        <body
-          className={`${inter.className} bg-gray-50 text-gray-900 min-h-screen flex flex-col`}
-          suppressHydrationWarning
-        >
-          <Navbar />
-          <main className="grow max-w-6xl mx-auto w-full p-4">{children}</main>
-        </body>
-      </html>
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-900 min-h-screen flex flex-col`}
+        suppressHydrationWarning
+      >
+        <Navbar />
+        <main className="grow max-w-6xl mx-auto w-full p-4">{children}</main>
+        <Analytics />
+      </body>
+    </html>
   );
 }
